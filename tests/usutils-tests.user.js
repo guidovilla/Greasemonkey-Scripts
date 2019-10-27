c11097c7ed6fbbbe8e620fe91ac7463c8d6e9c1a
Guido Villa <4699354-guidovilla@users.noreply.gitlab.com>
2019-10-27 11:23:52 +0000

    minor improvements to test suite

--- tests/usutils-tests.user.js
+++ tests/usutils-tests.user.js
@@ -12,7 +12,7 @@
 //
 // @match           https://www.google.com/*
 // @connect         google.com
-// @connect         www.guugle.com
+// @connect         nonexistant.domain.dont
 //
 // @require         https://greasyfork.org/scripts/391648-us-utils/code/US_Utils.js
 // @grant           GM_xmlhttpRequest
@@ -59,19 +59,19 @@
     var csv = 'a,"b","c\nc"\n"d,d","e""",4';
     tst('parseCSV', '2x3 array with: a b c<newline>c  d,d e" 4', UU.parseCSV(csv));
 
-    tst('wait','wait 10 seconds, then log info "value"', UU.wait(10000, 'value').then(UU.li));
-    tst('thenWait','log info "start", wait 15 second, log info "1234"');
+    tst('wait','wait 10 seconds, then log info "10 seconds have passed"', UU.wait(10000, '10 seconds have passed').then(UU.li));
+    tst('thenWait','log info "start", wait 15 second, log info "15 seconds have passed"');
     UU.li('start');
-    Promise.resolve(1234).then(UU.thenWait(15000)).then(UU.li);
+    Promise.resolve('15 seconds have passed').then(UU.thenWait(15000)).then(UU.li);
 
     i = 0;
     tst('GM_xhR ' + ++i,'log info downloaded www.google.com page');
     UU.GM_xhR('GET', 'http://www.google.com', 'test dnd ' + i).then(UU.li).catch(UU.le);
     tst('GM_xhR ' + ++i,'2x log error server not found');
-    UU.GM_xhR('GET', 'http://www.guugle.com', 'test dnd ' + i).then(UU.li).catch(UU.le);
+    UU.GM_xhR('GET', 'http://nonexistant.domain.dont', 'test dnd ' + i).then(UU.li).catch(UU.le);
     tst('GM_xhR ' + ++i,'2x log error 404 error');
     UU.GM_xhR('GET', 'http://google.com/ssdytrfddd', 'test dnd ' + i).then(UU.li).catch(UU.le);
     tst('GM_xhR ' + ++i,'log info downloaded www.google.com page, parsed document in responseXML2');
-    UU.GM_xhR('GET', 'http://www.google.com', 'test dnd ' + i, { 'responseType': 'document' }).then(function(resp){UU.li(resp.responseXML2.getElementsByTagName('body')[0].innerHTML);}).catch(UU.le);
+    UU.GM_xhR('GET', 'http://www.google.com', 'test dnd ' + i, { 'responseType': 'document' }).then(function(resp){UU.li(resp.context,'\n',resp.responseXML2.querySelector('body').innerHTML);}).catch(UU.le);
 
 }());
